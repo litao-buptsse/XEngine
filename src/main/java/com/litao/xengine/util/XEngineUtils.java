8169ae533d32ad03110a955086991900f21d5854
Tao Li <litao@sogou-inc.com>
2014-09-20 16:35:22 +0800

    Create server znode with path ${SERVER_IP}_${SESSION_ID}_${SEQUENCE_ID}

---
+++ src/main/java/com/litao/xengine/util/XEngineUtils.java
@@ -0,0 +1,51 @@
+package com.litao.xengine.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Enumeration;
+
+/**
+ * Created by Tao Li on 2014/9/20.
+ */
+public class XEngineUtils {
+    private static Logger LOG = LoggerFactory.getLogger(XEngineUtils.class);
+
+    public static String getRealIp() {
+        String localip = null;// 本地IP，如果没有配置外网IP则返回它
+        String netip = null;// 外网IP
+        try {
+            Enumeration<NetworkInterface> netInterfaces =
+                    NetworkInterface.getNetworkInterfaces();
+            InetAddress ip = null;
+            boolean finded = false;// 是否找到外网IP
+            while (netInterfaces.hasMoreElements() && !finded) {
+                NetworkInterface ni = netInterfaces.nextElement();
+                Enumeration<InetAddress> address = ni.getInetAddresses();
+                while (address.hasMoreElements()) {
+                    ip = address.nextElement();
+                    if (!ip.isSiteLocalAddress()
+                            && !ip.isLoopbackAddress()
+                            && ip.getHostAddress().indexOf(":") == -1) {// 外网IP
+                        netip = ip.getHostAddress();
+                        finded = true;
+                        break;
+                    } else if (ip.isSiteLocalAddress()
+                            && !ip.isLoopbackAddress()
+                            && ip.getHostAddress().indexOf(":") == -1) {// 内网IP
+                        localip = ip.getHostAddress();
+                    }
+                }
+            }
+        } catch (Exception e) {
+            LOG.error("Get IP Failed");
+        }
+        if (netip != null && !"".equals(netip)) {
+            return netip;
+        } else {
+            return localip;
+        }
+    }
+}
--- src/main/java/com/litao/xengine/zookeeper/XEngineWatcher.java
+++ src/main/java/com/litao/xengine/zookeeper/XEngineWatcher.java
@@ -1,8 +1,10 @@
 package com.litao.xengine.zookeeper;
 
 import com.litao.xengine.config.XEngineConfiguration;
+import com.litao.xengine.util.XEngineUtils;
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.ZooDefs;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,30 +16,37 @@ import java.util.concurrent.TimeUnit;
  * Created by Tao Li on 9/16/14.
  */
 public class XEngineWatcher extends ConnectionWatcher {
-    private static String ZOOKEEPER_HOSTS = null;
-    private static String ROOT_PATH = null;
-    private static String SERVER_NAME = null;
-    private static String SERVERS_PATH = null;
-    private static String SERVER_PATH = null;
+    private static final String ZOOKEEPER_HOSTS;
+    private static final String ROOT_PATH;
+    private static final String SERVERS_PATH;
+
+    public static final String SERVER_IP;
+    private static final String SERVER_NAME;
+    private static final String SERVER_BASE_PATH;
+
+    private static volatile String serverNodePath = null;
 
     private static XEngineWatcher instance = null;
 
     private static Logger LOG = LoggerFactory.getLogger(XEngineWatcher.class);
 
     static {
+        SERVER_IP = XEngineUtils.getRealIp();
+
         ZOOKEEPER_HOSTS = XEngineConfiguration.CONFIG.getString("xengine.zookeeper.quorum");
         ROOT_PATH = XEngineConfiguration.CONFIG.getString("xengine.zookeeper.root.path", "/xengine");
-        SERVER_NAME = XEngineConfiguration.CONFIG.getString("xengine.zookeeper.server.name");
+        SERVER_NAME = XEngineConfiguration.CONFIG.getString("xengine.zookeeper.server.name", SERVER_IP);
 
         if (ZOOKEEPER_HOSTS == null || ZOOKEEPER_HOSTS.equals("")
                 || ROOT_PATH == null || ROOT_PATH.equals("")
-                || SERVER_NAME == null || SERVER_NAME.equals("")) {
+                || SERVER_NAME == null || SERVER_NAME.equals("")
+                || SERVER_IP == null || SERVER_IP.equals("")) {
             LOG.error("Basic configuration is failed to load.");
             System.exit(1);
         }
 
         SERVERS_PATH = ROOT_PATH + "/servers";
-        SERVER_PATH = SERVERS_PATH + "/" + SERVER_NAME;
+        SERVER_BASE_PATH = SERVERS_PATH + "/" + SERVER_NAME;
     }
 
     public static XEngineWatcher getInstance() {
@@ -55,21 +64,27 @@ public class XEngineWatcher extends ConnectionWatcher {
     private XEngineWatcher() {
         try {
             connect(ZOOKEEPER_HOSTS);
-            if (zk.exists(ROOT_PATH, false) == null) {
-                zk.create(ROOT_PATH, null, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
-            }
-            if (zk.exists(SERVERS_PATH, false) == null) {
-                zk.create(SERVERS_PATH, null, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
-            }
-            register();
+            prepare();
+            serverNodePath = register();
         } catch (Exception e) {
             LOG.error(e.getMessage(), e);
             System.exit(1);
         }
     }
 
+    private void prepare() throws KeeperException, InterruptedException {
+        // Create root basic znode
+        if (zk.exists(ROOT_PATH, false) == null) {
+            zk.create(ROOT_PATH, null, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+        }
+        if (zk.exists(SERVERS_PATH, false) == null) {
+            zk.create(SERVERS_PATH, null, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+        }
+    }
+
     private String register() throws KeeperException, InterruptedException {
-        return zk.create(SERVER_PATH, null, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+        String znodePath = SERVER_BASE_PATH + "_" + zk.getSessionId() + "_";
+        return zk.create(znodePath, null, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
     }
 
     public List<String> getActiveServers() throws KeeperException, InterruptedException {
@@ -77,9 +92,8 @@ public class XEngineWatcher extends ConnectionWatcher {
         return zk.getChildren(SERVERS_PATH, false);
     }
 
-    public static void main(String[] args) throws InterruptedException {
-        XEngineWatcher watcher = XEngineWatcher.getInstance();
-        TimeUnit.SECONDS.sleep(20);
-        watcher.close();
+    @Override
+    protected void handler(WatchedEvent event) {
+
     }
 }
